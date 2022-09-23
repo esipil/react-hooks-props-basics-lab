66fb7b0a941b9338884452dbeb6548fddf885ded
Timothy <lemayiant4@gmail.com>
2022-09-23 14:10:43 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,23 @@
 import React from "react";
 
-function About() {
+function About(props) {
+  function rendering () {
+    if (props.bio) {
+      return <p> {props.bio}</p>
+    } else {
+      return ''
+    }
+  }
+
+
+
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me </h2>
+      { rendering()}
+      {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+    </div> 
   );
 }
 
